42b733961f2793fde7fd311233819182b93fcb63
Jaro233 <jaroslaw.waliczek@vp.pl>
2023-11-02 17:29:43 +0100

    first commit

---
+++ vars.tf
@@ -0,0 +1,31 @@
+variable "name" {
+  type    = string
+}
+
+variable "region" {
+  type    = string
+}
+
+variable "vpc_cidr" {
+  type    = string
+}
+
+variable "azs" {
+  type    = list(string)
+}
+
+variable "public_subnets" {
+  type    = list(string)
+}
+
+variable "private_subnets" {
+  type    = list(string)
+}
+
+variable "intra_subnets" {
+  type    = list(string)
+}
+
+variable "tags" {
+  type = map(string)
+}
